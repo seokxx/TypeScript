1a65830660cfbd03034915cb8df3ae3872aefd80
www.seok.com <qhtjr7583@naver.com>
2023-07-02 04:24:37 +0900

    (2강) union, any, unknown types
    타입을 미리 정하기 애매한 경우 사용.

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-let 이름 : string = 'kim'; //string
-let 나이 : number = 50; //number
+//기본타입
+let 이름 : string = "Hong"; //string
+let 나이 : number = 24; //number
 let 결혼했니 : boolean = true; //boolean
-let 회원들 : string[] = ['kim', 'park'] //list
+let 회원들 : (string|number)[] = ['kim', 123] //list
 let 회원들2 : { member1 :string, member2:string } = { member1 :'kim', member2:'park' } //object
 let 회원들3 : 'park' //타입 생략가능
 
@@ -23,3 +24,46 @@ let project :{
     days : 30,
     started : true,
   }
+
+//union타입
+let 이름2: string | number = 'hong';
+let 나이2: (string | number) = 20;
+
+let 회원들4 : (number | string )[] = [1,'2',3];
+let 오브젝트 : {a : string | number} = {a : '123'}
+
+var 배열: (number | string)[] = [1,'2',3]
+
+//any타입
+let 이름3: any = 'hong';
+이름3 = 123;
+이름3 = false;
+이름3 = [];
+
+//unknown타입
+let 이름4 : unknown = "hong";
+이름4 = 123;
+이름4 = false;
+이름4 = {};
+//이름4 -1;
+
+//let 변수1 : string = 이름4;
+
+//Q1
+let user:string = 'kim';
+let age:undefined | number = undefined;
+let married:boolean = false; 
+let 철수:(string | undefined | boolean)[] = [user, age, married];
+
+//Q2
+let 학교: {
+    score : (number|boolean)[],
+    teacher : string,
+    friend : string | string[]
+} = {
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
