dce97bf014a2326b5be874de6270ae85f44ebc80
www.seok.com <qhtjr7583@naver.com>
2023-07-04 03:28:26 +0900

    (16강) JSX
    리액트 타입지정 문법

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-
-import './class15';
+import React, {useState} from 'react';
 
 function App(){
+
+let [user, setUser] = useState<string |number>('hong')
+
   return (
     <div>
-      
+      <h4>안녕하십니까</h4>
+      <Profile name="철수" age="20"></Profile>
     </div>
   );
+}
+
+function Profile(props:{name:string, age:string}) :JSX.Element{
+  return(
+    <div>{props.name}입니다.</div>
+  )
 }
 
 export default App;
--- src/class15.tsx
+++ src/class15.tsx
@@ -1,26 +1,27 @@
-function 함수<T>(x: T[]):T {
-    return x[0]   
-}
+// function 함수<T>(x: T[]):T {
+//     return x[0]   
+// }
 
-let a = 함수<number>([4,2])
-let b = 함수<string>(['4','2'])
+// let a = 함수<number>([4,2])
+// let b = 함수<string>(['4','2'])
 
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 
-function 함수2<T extends number> (x: T){
-    return x -1 
-}
+// function 함수2<T extends number> (x: T){
+//     return x -1 
+// }
 
-let c = 함수2<number>(100)
-console.log(c)
-interface LengthCheck{
-    length: number
-}
+// let c = 함수2<number>(100)
+// console.log(c)
 
-function 함수3<T extends LengthCheck> (x: T){
-    return x.length 
-}
+// interface LengthCheck{
+//     length: number
+// }
 
-let d = 함수3<string>('abcde')
-console.log(d)
+// function 함수3<T extends LengthCheck> (x: T){
+//     return x.length 
+// }
+
+// let d = 함수3<string>('hello')
+// console.log(d)
