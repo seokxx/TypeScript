e7c3bd5a8c525c1d6aa9c87337e74e4180acceb2
www.seok.com <qhtjr7583@naver.com>
2023-07-04 07:00:29 +0900

    (18걍) tuple homework
    tuple 과제

--- src/class18.tsx
+++ src/class18.tsx
@@ -9,3 +9,25 @@ function 함수(...x: [number, string, ...number[]]) {
 
 let arr = [1, 2, 3];
 let arr2: [number, number, ...number[]] = [4, 5, ...arr];
+
+//q1
+type Food = [string, number, ...boolean[]];
+//q2
+let food: Food = ["spaghetti", 12000, true, false, true];
+//q3
+function 함수2(...x: [string, boolean, ...(number | string)[]]) {}
+함수2("a", true, 6, 3, "1", 4);
+//q4
+function 분류기(...x: [...(string | number)[]]) {
+  let result: [string[], number[]] = [[], []];
+
+  x.forEach((a) => {
+    if (typeof a === "string") {
+      result[0].push(a);
+    } else {
+      result[1].push(a);
+    }
+    return result;
+  });
+}
+console.log(분류기("a", 3, "d", 4, 5));
