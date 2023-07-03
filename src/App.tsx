0811f1bad088a93c38a243d4047126faed23aa79
www.seok.com <qhtjr7583@naver.com>
2023-07-04 05:10:26 +0900

    (17강) redux
    state 저장소

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,53 @@
-import React, {useState} from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  RootState,
+  AppDispatch,
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+} from "./index";
 
-function App(){
-
-let [user, setUser] = useState<string |number>('hong')
-
+function App() {
+  const state = useSelector((state: RootState) => state);
+  const dispatch = useDispatch<AppDispatch>();
+  const getRandom = (min: number, max: number) =>
+    Math.floor(Math.random() * (max - min) + min);
+  let rand = getRandom(1, 100);
   return (
-    <div>
-      <h4>안녕하십니까</h4>
-      <Profile name="철수" age="20"></Profile>
+    <div className="App">
+      {state.counter1.count} <br />
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        증가
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        감소
+      </button>
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(rand));
+        }}
+      >
+        랜덤증가
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrementByAmount(rand));
+        }}
+      >
+        랜덤감소
+      </button>
     </div>
   );
-}
-
-function Profile(props:{name:string, age:string}) :JSX.Element{
-  return(
-    <div>{props.name}입니다.</div>
-  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { createSlice, configureStore, PayloadAction } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+interface Count { count: number, user: string}
+
+const initialState: Count = { count: 0, user : 'hong' };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState : initialState,
+  reducers: {
+    increment (state){
+      state.count += 1
+    },
+    decrement (state){
+      state.count -= 1
+    },
+    incrementByAmount (state, action :PayloadAction<number>){
+      state.count += action.payload
+    },
+    decrementByAmount (state, action :PayloadAction<number>){
+      state.count -= action.payload
+    }
+  }
+})
+
+let store = configureStore({
+  reducer: {
+    counter1 : counterSlice.reducer
+  }
+})
+
+//useSelector 타입지정
+export type RootState = ReturnType<typeof store.getState>
+//useDispatch 타입지정
+export type AppDispatch = typeof store.dispatch
+//만든 함수 export
+export let {increment, decrement, incrementByAmount, decrementByAmount} = counterSlice.actions
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
